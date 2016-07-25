2d1ce3ae7ff82af8d9a5afd3e17136f54039e34f
Alessandro Boch <aboch@docker.com>
2016-07-25 01:52:48 -0700

    Properly handle advertise address locality
    - Current encryption code assumes the advertise address
      is not a local address
    
    Signed-off-by: Alessandro Boch <aboch@docker.com>

--- drivers/overlay/encryption.go
+++ drivers/overlay/encryption.go
@@ -90,6 +90,9 @@ func (d *driver) checkEncryption(nid string, rIP net.IP, vxlanID uint32, isLocal
 
 	lIP := net.ParseIP(d.bindAddress)
 	aIP := net.ParseIP(d.advertiseAddress)
+	if d.advAddrIsLocal {
+		lIP = aIP
+	}
 	nodes := map[string]net.IP{}
 
 	switch {
@@ -413,6 +416,10 @@ func (d *driver) updateKeys(newKey, primary, pruneKey *key) error {
 		delIdx = -1
 		lIP    = net.ParseIP(d.bindAddress)
 	)
+
+	if d.advAddrIsLocal {
+		lIP = net.ParseIP(d.advertiseAddress)
+	}
 
 	d.Lock()
 	// add new
